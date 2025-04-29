51dc8588497e8ae81165e83e293d8d34ef5ed1a1
Felipe <luiz.VitorADS@outlook.com>
2025-04-29 08:38:10 -0300

    modificação do banco de dados para mongo altlas

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,8 @@
-const moongose = require('mongoose')
+const mongose = require('mongoose')
+require('dotenv').config()
+const dbpass = process.env.DBPASSWORD
+const userdb = process.env.DBUSER
 
-const db =moongose.connect("mongodb://127.0.0.1:27017/aiDatabase")
+const db =mongose.connect('mongodb+srv://'+userdb+':'+dbpass+'@cluster0.l1lwgre.mongodb.net/')
 
  module.exports = db
